1361d47c6a32d0661b90a9c6b9150a449da586ff
Hassan Ghaseminia <ghasminia@gmail.com>
2024-08-14 15:51:31 +0330

    feat: implement sshpass for sftp connection

--- lsync/lsyncd.conf.lua
+++ lsync/lsyncd.conf.lua
@@ -5,13 +5,16 @@ settings {
 }
 
 sync {
-    default.rsync,
+    default.rsyncssh,
     source = "/source",
-    target = "/target",
+    host = "root@your_ip",
+    targetdir = "/backups/server-131/duplicati",
     rsync = {
         archive = true,
         compress = false,
         verbose = true,
-        _extra = {"--delete"}
+        binary = "/usr/bin/sshpass",
+        _extra = {"-p", "password", "rsync", "-e", "ssh -p 22", "--delete"}
+
     }
 }
